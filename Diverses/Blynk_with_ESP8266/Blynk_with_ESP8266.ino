b31ce21698529b8e46913d5c7d727a91df19f098
DendeXx <39348073+DendeXx@users.noreply.github.com>
2019-06-04 20:39:22 +0200

    Update Blynk_with_ESP8266.ino
    Co-Authored-By: TobiasFr <frahm.tobias@gmail.com>

--- Diverses/Blynk_with_ESP8266/Blynk_with_ESP8266.ino
+++ Diverses/Blynk_with_ESP8266/Blynk_with_ESP8266.ino
@@ -3,15 +3,20 @@
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <HX711.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 char auth[] = "0e310d6d3f984c7f90a4b0dc8f9cd2a6";
+const int LOADCELL_DOUT_PIN = D1;
+const int LOADCELL_SCK_PIN = D2;
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
+//WiFi
+
 char ssid[] = "Nimmerland";
 char pass[] = "7Uzgl4#1";
+
+//HX711 scale;
 
 void setup()
 {
